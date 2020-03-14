4a027704a91e60705e20e14ecc793092bc54efed
jeb <saldariega.jam@gmail.com>
2020-03-14 13:08:19 +0800

    clear files

--- src/views/list.blade.php
+++
@@ -1,8 +0,0 @@
-<h3>Submissions</h3>
-
-@foreach ($list as $item)
-{{ $item->name }} says {{ $item->message }} <a href="contactus/{{ $item->id }}/edit">edit</a>
-    <br />
-@endforeach
-<br /><br />
-<a href="/contactus/create">Contact Us</a>
